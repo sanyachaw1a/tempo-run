89c278ff8457c9708eded41584cc71cf5ad8016a
sanyachaw1a <87011400+sanyachaw1a@users.noreply.github.com>
2025-03-31 23:14:17 -0400

    Initial commit - Xcode project setup

---
+++ tempo-run/tempo-run/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tempo-run
+//
+//  Created by Sanya Chawla on 3/31/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tempo-run/tempo-run/tempo_runApp.swift
@@ -0,0 +1,17 @@
+//
+//  tempo_runApp.swift
+//  tempo-run
+//
+//  Created by Sanya Chawla on 3/31/25.
+//
+
+import SwiftUI
+
+@main
+struct tempo_runApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
